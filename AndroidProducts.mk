e5913f8335f16d95b991fb0fb962a21b943d5e79
DigiGoon <kagatharatarang@gmail.com>
2023-03-27 03:14:58 +0530

    haydn: Init P404

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_haydn.mk
+    $(LOCAL_DIR)/p404_haydn.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_haydn-user \
-    lineage_haydn-userdebug \
-    lineage_haydn-eng
+    p404_haydn-user \
+    p404_haydn-userdebug \
+    p404_haydn-eng
