85a6dda31c5a25d1354601db8946f4282efe3096
AdarshGrewal <adarshgrewal@gmail.com>
2023-06-29 19:41:10 +0530

    haydn: Include Leica Camera if exists

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -37,4 +37,14 @@ rm -rf hardware/qcom-caf/sm8350/display && git clone https://github.com/LineageO
 echo 'Cloning firmware'
 git clone https://gitlab.com/Alucard_Storm/vendor_xiaomi_haydn-firmware -b thirteen vendor/xiaomi/haydn-firmware
 
+# Camera
+echo 'Cloning Leica Camera'
+git clone https://gitlab.com/Alucard_Storm/haydn-miuicamera -b thirteen-leica vendor/xiaomi/haydn-miuicamera
+
+echo 'Adding slow-mo fix for Leica Camera'
+cd hardware/interfaces
+wget https://github.com/Astridxx/android_hardware_interfaces/commit/ca6551863fb82110688452981572d44a26806681.patch
+patch -p1 <ca6551863fb82110688452981572d44a26806681.patch
+cd ../..
+
 echo 'delete vendorsetup.sh from device tree once this is done'
