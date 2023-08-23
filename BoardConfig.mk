d1b24691f17f2c837cba302da8f74b9dd5986868
AdarshGrewal <adarshgrewal@gmail.com>
2023-08-23 14:35:26 +0300

    haydn: Include Leica Camera if exists

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,3 +39,6 @@ SOONG_CONFIG_XIAOMI_VIBRATOR_USE_EFFECT_STREAM := true
 
 # Include proprietary files
 include vendor/xiaomi/haydn/BoardConfigVendor.mk
+
+# Inherit from proprietary files for Leica Camera
+-include vendor/xiaomi/haydn-miuicamera/products/board.mk
--- device.mk
+++ device.mk
@@ -45,3 +45,6 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/haydn/haydn-vendor.mk)
+
+# Call the Leica Camera setup
+$(call inherit-product-if-exists, vendor/xiaomi/haydn-miuicamera/products/miuicamera.mk)
