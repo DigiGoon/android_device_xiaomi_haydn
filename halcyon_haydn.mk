c5fd70cf6568c7d810d4db5747911c74bc6fd1e8
DigiGoon <kagatharatarang@gmail.com>
2023-09-16 04:51:50 +0530

    haydn: Init halcyon
    Signed-off-by: DigiGoon <kagatharatarang@gmail.com>

--- halcyon_haydn.mk
+++ halcyon_haydn.mk
@@ -11,14 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from haydn device
 $(call inherit-product, device/xiaomi/haydn/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common halcyon stuff.
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := haydn
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2012K11G
-PRODUCT_NAME := lineage_haydn
+PRODUCT_NAME := halcyon_haydn
 
 PRODUCT_SYSTEM_NAME := haydn_global
 PRODUCT_SYSTEM_DEVICE := haydn
@@ -31,3 +31,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := Xiaomi/haydn_global/haydn:13/TKQ1.220829.002/V14.0.3.0.TKKMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# halcyon
+TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GMS := true
