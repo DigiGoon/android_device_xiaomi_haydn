7ca497f9f250781a872eeae335232b56dc686c25
DigiGoon <kagatharatarang@gmail.com>
2024-07-22 19:05:27 +0530

    haydn: Init EverestOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_haydn.mk
+    $(LOCAL_DIR)/everest_haydn.mk
--- everest_haydn.mk
+++ everest_haydn.mk
@@ -12,13 +12,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/haydn/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/everest/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := haydn
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2012K11G
-PRODUCT_NAME := lineage_haydn
+PRODUCT_NAME := everest_haydn
 
 PRODUCT_SYSTEM_NAME := haydn_global
 PRODUCT_SYSTEM_DEVICE := haydn
@@ -31,3 +31,17 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := Xiaomi/haydn_global/haydn:14/UKQ1.231207.002/V816.0.2.0.UKKMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# EverestOS Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_64_BIT_APPS := true
+WITH_GMS := true
+WITH_GAPPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+EVEREST_MAINTAINER := DigiGoon
+TARGET_SUPPORTS_BLUR := true
+TARGET_HAS_UDFPS := false
+EXTRA_UDFPS_ANIMATIONS := false
+TARGET_INCLUDE_PIXEL_LAUNCHER := false
+TARGET_INCLUDE_LAUNCHER3 := true
