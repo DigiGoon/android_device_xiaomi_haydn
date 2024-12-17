6a0f764e0c726e00dd8c07996dc267ee8bf0a9a3
DigiGoon <kagatharatarang@gmail.com>
2024-12-17 20:08:20 +0530

    haydn: Init RisingOS

--- lineage_haydn.mk
+++ lineage_haydn.mk
@@ -31,3 +31,39 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := Xiaomi/haydn_global/haydn:14/UKQ1.231207.002/V816.0.6.0.UKKMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Lunch banner maintainer variable
+RISING_MAINTAINER="DigiGoon"
+
+# Chipset/Maintainer properties (ro.rising.chipset/ro.rising.maintainer)
+# (Optional if builder is setting properties via init_<device>.cpp)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Snapdragon® 888" \
+    RisingMaintainer="DigiGoon"
+
+# chipset flag enclose var with "" if more than one
+# this will reflect on build/display version, a firmware package/zip name
+# e.g. risingOS-6.0-COMMUNITY-device-AOSP.zip - AOSP is the default package type, WITH_GMS will override the package type to PIXEL
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+
+# whether to ship aperture camera, default is false
+PRODUCT_NO_CAMERA := true
+
+# Wether to ship lawnchair launcher
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+# GMS build flags
+# ship with GMS packages, replaces default AOSP packages with Google manufactured packages.
+WITH_GMS := true
+
+# on the other hand builders can build customize packages by simply defining product packages
+# instead of defining TARGET_CORE_GMS_EXTRAS
+# PRODUCT_PACKAGES += \
+#    add package name here e.g: Velvet \
+#    LatinIMEGooglePrebuilt (if builder prefers gboard)
+
+# Wether to ship pixel launcher and set it as default launcher
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+TARGET_EXCLUDES_AUDIOFX := true
